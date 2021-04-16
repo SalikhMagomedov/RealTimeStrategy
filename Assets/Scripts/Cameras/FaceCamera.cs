57bd0e8360d0924f95e887f855cfed769832c89a
Salikh Magomedov <salikh.magomedov@icloud.com>
2021-04-16 12:22:53 +0300

    Buildings Setup

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Rts.Cameras
+{
+    public class FaceCamera : MonoBehaviour
+    {
+        private Transform _mainCameraTransform;
+
+        private void Start()
+        {
+            if (Camera.main is { }) _mainCameraTransform = Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            var rotation = _mainCameraTransform.rotation;
+            transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+        }
+    }
+}
